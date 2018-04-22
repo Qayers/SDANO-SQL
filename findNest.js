ae0bc13a4812ba30858d74e1e3edfa3ebed13611
Andrzej Kulesza <qayers.dev@gmail.com>
2018-04-22 15:08:20 +0200

    rozwiązanie zad1A2

--- findNest.js
+++ findNest.js
@@ -1,18 +1,6 @@
 use films
 
- // db.film.insert(
-//	{"title":"Sami Swoi",
-	//"year":1974,
-	// "director":
-		// {"first_name":"Sylwester",
-		// "last_name":"Chęciński",
-		// "birthday":"1926-11-22"}
-		//}
-		
-	// )
-		
-		// db.film.find()
-		
+ db.film.find();	
 	
 	db.film.aggregate(
 	[
@@ -22,6 +10,7 @@ use films
 		}
 	}
 	}
+	]
 	)
 	
 	
